6b82ba7e9815588ce9f6533d477983b4c3016855
Shubham  Kumar <sk@Shubhams-Laptop.local>
2025-07-14 06:30:21 +0530

    worked on blog-rag

---
+++ models/search.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+
+class Query(BaseModel):
+    query: str = Field(..., min_length=3)
+
+
+class Context(BaseModel):
+    documentid: str
+    content: str
+    similarity: float
+
+
+class ResponseData(BaseModel):
+    thought_process: List[str] = []
+    answer: str = ""
+    enough_context: bool = True
+
+
+class AiResponseData(BaseModel):
+    thought_process: List[str]
+    answer: str
+    enough_context: bool
+    recomendant_blog_id: str
+    recomendant_product_name: str
+
+
+class SearchResponse(BaseModel):
+    message: str
+    results: List[Context]
+
+
+class RelatedQuestionRequest(BaseModel):
+    question: str = Field(..., min_length=3)
+    context: Optional[str] = None
+
+class RelatedQuestionResponse(BaseModel):
+    related_questions: list[str]
